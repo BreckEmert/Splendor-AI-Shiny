Fixed masking, dueling, and vectorized more training

Splendor/Environment/Splendor_components/Player_components/player.py
@@ -31,8 +31,8 @@ class Player:
 
     def choose_discard(self, state, player_gems, progress=0, reward=0, move_index=None):
         # Set legal mask to only legal discards
-        legal_mask = np.zeros(61, dtype=int)
-        legal_mask[10:15] = player_gems[:5]
+        legal_mask = np.zeros(61, dtype=bool)
+        legal_mask[10:15] = player_gems[:5] > 0
 
         if not move_index:
             # Call the model to choose a discard
@@ -47,7 +47,7 @@ class Player:
         next_state = state.copy()
         next_state[gem_index+self.state_offset] -= 0.25
         state[195] = 0.2*progress # 0.2 * (moves remaining+1), indicating progression through loop
-        self.rl_model.remember([state.copy(), move_index, reward, next_state.copy(), False], legal_mask.copy())
+        self.rl_model.remember([state.copy(), move_index, reward, next_state.copy(), 1], legal_mask.copy())
 
         # Update player in the game state (not board anymore)
         state = next_state.copy() # Do we need to do .copy()
@@ -56,8 +56,8 @@ class Player:
     
     def choose_take(self, state, available_gems, progress, reward=0, take_index=None):
         # Set legal mask to only legal takes
-        legal_mask = np.zeros(61, dtype=int)
-        legal_mask[:5] = available_gems
+        legal_mask = np.zeros(61, dtype=bool)
+        legal_mask[:5] = available_gems > 0
 
         if not take_index:
             # Call the model to choose a take
@@ -71,7 +71,7 @@ class Player:
         next_state = state.copy()
         next_state[take_index+self.state_offset] += 0.25
         state[195] = progress # 0.2 * (moves remaining+1), indicating progression through loop
-        self.rl_model.remember([state.copy(), take_index, reward, next_state.copy(), False], legal_mask.copy())
+        self.rl_model.remember([state.copy(), take_index, reward, next_state.copy(), 1], legal_mask.copy())
 
         return take, next_state
 
@@ -127,7 +127,7 @@ class Player:
 
             # Legal tokens to spend
             legal_mask[10:15] = (gems*cost != 0)[:5] # Can only spend gems where card cost remains
-            legal_mask[60] = 1 if gems[5] else 0 # Enable spending gold as a legal move
+            legal_mask[60] = True if gems[5] else False # Enable spending gold as a legal move
 
             # Predict token to spend
             rl_moves = self.rl_model.get_predictions(state, legal_mask)
@@ -137,7 +137,7 @@ class Player:
             # Remember
             next_state = state.copy()
             next_state[gem_index+self.state_offset] -= 0.25
-            self.rl_model.remember([state.copy(), move_index, 0, next_state.copy(), False], legal_mask.copy())
+            self.rl_model.remember([state.copy(), move_index, 0, next_state.copy(), 1], legal_mask.copy())
 
             # Update player in game state
             state = next_state.copy()
@@ -216,30 +216,30 @@ class Player:
         return legal_moves
     
     def legal_to_vector(self, legal_moves):
-        legal_mask = np.zeros(61, dtype=int)
+        legal_mask = np.zeros(61, dtype=bool)
         for move, details in legal_moves:
             tier, card_index = details
             match move:
                 case 'take':
                     gem, amount = details # Overriding tier and card_index
                     if amount == 1:
-                        legal_mask[gem] = 1
+                        legal_mask[gem] = True
                     elif amount == 2:
-                        legal_mask[gem+5] = 1
+                        legal_mask[gem+5] = True
                     elif amount == -1:
-                        legal_mask[gem+10] = 1
+                        legal_mask[gem+10] = True
                 case 'buy':
-                    legal_mask[15 + 4*tier + card_index] = 1
+                    legal_mask[15 + 4*tier + card_index] = True
                 case 'buy reserved':
-                    legal_mask[27 + card_index] = 1
+                    legal_mask[27 + card_index] = True
                 case 'buy with gold':
-                    legal_mask[30 + 4*tier + card_index] = 1
+                    legal_mask[30 + 4*tier + card_index] = True
                 case 'buy reserved with gold':
-                    legal_mask[42 + card_index] = 1
+                    legal_mask[42 + card_index] = True
                 case 'reserve':
-                    legal_mask[45 + 4*tier + card_index] = 1
+                    legal_mask[45 + 4*tier + card_index] = True
                 case 'reserve top':
-                    legal_mask[57 + tier] = 1
+                    legal_mask[57 + tier] = True
 
         return legal_mask
     
@@ -255,7 +255,7 @@ class Player:
                 gem_index = move_index % 5
                 next_state = state.copy()
                 next_state[gem_index+self.state_offset] += 0.5
-                self.rl_model.remember([state.copy(), move_index, -2/15, next_state.copy(), False], legal_mask.copy())
+                self.rl_model.remember([state.copy(), move_index, -2/15, next_state.copy(), 1], legal_mask.copy())
 
                 chosen_gems = np.zeros(6, dtype=int)
                 chosen_gems[gem_index] = 2
@@ -279,14 +279,15 @@ class Player:
 
             if self.points+points >= 15:
                 reward += 10
-                self.rl_model.remember([state.copy(), move_index, reward, state.copy(), True], legal_mask.copy())
+                self.rl_model.remember([state.copy(), move_index, reward, state.copy(), 0], legal_mask.copy())
+                self.rl_model.memory[-1].append(legal_mask.copy())
                 self.victor = True
                 return None
 
             next_state = state.copy()
             offset = 11 * (4*tier + card_index)
             next_state[offset:offset+11] = board.deck_mapping[tier].peek_vector()
-            self.rl_model.remember([state.copy(), move_index, reward, next_state.copy(), False], legal_mask.copy())
+            self.rl_model.remember([state.copy(), move_index, reward, next_state.copy(), 1], legal_mask.copy())
             
             if move_index < 27:
                 move = ('buy', (tier, card_index))
@@ -312,7 +313,7 @@ class Player:
             # Remember
             next_state = state.copy()
             next_state[offset:offset+11] = board.deck_mapping[tier].peek_vector()
-            self.rl_model.remember([state.copy(), move_index, -2/15, next_state.copy(), False], legal_mask.copy())
+            self.rl_model.remember([state.copy(), move_index, -2/15, next_state.copy(), 1], legal_mask.copy())
         
         return move
     
@@ -322,6 +323,7 @@ class Player:
         rl_moves = self.rl_model.get_predictions(state, legal_mask)
         
         self.move_index = np.argmax(rl_moves)
+        print("player.choose_move move index:", self.move_index)
         self.chosen_move = self.vector_to_details(state, board, legal_mask, self.move_index)
         return self.chosen_move
     

Splendor/RL/model.py
@@ -19,7 +19,7 @@ class RLAgent:
         self.game_length = 0
         self.batch_size = 128
 
-        self.gamma = 1 # 0.1**(1/25)
+        self.gamma = 0.99 # 0.1**(1/25)
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.04
         self.epsilon_decay = 0.99
@@ -84,8 +84,6 @@ class RLAgent:
             act_values = np.random.rand(self.action_size)  # Exploration
         else:
             act_values = self.model.predict(state, verbose=0)[0]  # All actions
-            # if np.random.rand() < 0.0004:
-            #     print(act_values[legal_mask==1])
 
         # Illegal move filter
         act_values = np.where(legal_mask, act_values, -np.inf) # Compatible with graph?
@@ -102,22 +100,27 @@ class RLAgent:
         actions = tf.convert_to_tensor([mem[1] for mem in batch], dtype=tf.int32) # Int8 or 16?
         rewards = tf.convert_to_tensor([mem[2] for mem in batch], dtype=tf.float32)
         next_states = tf.convert_to_tensor([mem[3] for mem in batch], dtype=tf.float32)
-        dones = tf.convert_to_tensor([mem[4] for mem in batch], dtype=bool) # Int8 or 16?
-        legal_masks = tf.convert_to_tensor([mem[5] for mem in batch], dtype=bool)
+        dones = tf.convert_to_tensor([mem[4] for mem in batch], dtype=tf.float32)
+        legal_masks = tf.convert_to_tensor([mem[5] for mem in batch], dtype=tf.bool)
 
+        # Calculate this turn's qs with primary model
         qs = self.model.predict(states, verbose=0)
-        target_qs = qs.copy()
-        next_qs = self.model.predict(next_states, verbose=0)
-        next_targets = self.target_model.predict(next_states, verbose=0)
-        
 
-        # Set q-values
-        for i in range(len(batch)):
-            if dones[i]:
-                target_qs[i][actions[i]] = rewards[i]
-            else:
-                best_next_q = tf.argmax(next_qs[i])
-                target_qs[i][actions[i]] = rewards[i] + self.gamma*next_targets[i][best_next_q]
+        # Predict next turn's actions with primary model
+        next_actions = self.model.predict(next_states, verbose=0)
+        next_actions = tf.where(legal_masks, next_actions, tf.fill(next_actions.shape, -np.inf))
+        next_actions = tf.argmax(next_actions, axis=1, output_type=tf.int32)
+        next_actions = tf.stack([tf.range(len(next_actions), dtype=tf.int32), next_actions], axis=1)
+
+        # Calculate next turn's qs with target model
+        next_qs = self.target_model.predict(next_states, verbose=0)
+        next_qs = tf.where(legal_masks, next_qs, tf.fill(next_qs.shape, -np.inf))
+        next_qs = tf.gather_nd(next_qs, next_actions)
+
+        # Ground qs with reward and value trajectory
+        targets = rewards + dones * self.gamma * next_qs
+        actions_indices = tf.stack([tf.range(len(actions), dtype=tf.int32), actions], axis=1)
+        target_qs = tf.tensor_scatter_nd_update(qs, actions_indices, targets)
 
         # Fit
         history = self.model.fit(states, target_qs, batch_size=256, epochs=1, verbose=0)
@@ -130,13 +133,14 @@ class RLAgent:
                 # Grouped cards
                 tf.summary.scalar('Training Metrics/batch_loss', history.history['loss'][0], step=step)
                 tf.summary.scalar('Training Metrics/avg_reward', tf.reduce_mean(rewards), step=step)
-                tf.summary.scalar('Training Metrics/avg_q', tf.reduce_mean(qs), step=step)
+                legal_qs = tf.where(tf.math.is_finite(qs), qs, tf.zeros_like(qs))
+                tf.summary.scalar('Training Metrics/avg_q', tf.reduce_mean(legal_qs), step=step)
                 tf.summary.histogram('Training Metrics/action_hist', actions, step=step)
 
                 # Q-values over time
                 for action in range(self.action_size):
-                    average_qs = np.mean(qs, axis=0)
-                    tf.summary.scalar(f"action_qs/action_{action}", average_qs[action], step=step)
+                    average_qs = np.mean(legal_qs[:, action], axis=0)
+                    tf.summary.scalar(f"action_qs/action_{action}", average_qs, step=step)
                 
                 # Weights
                 for layer in self.model.layers:
@@ -146,9 +150,6 @@ class RLAgent:
 
     def replay(self):
         # Training twice for now
-        batch = sample(self.memory, self.batch_size)
-        self._batch_train(batch)
-        
         batch = sample(self.memory, self.batch_size)
         self._batch_train(batch)
         